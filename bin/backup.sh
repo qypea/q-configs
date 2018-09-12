2d58a46e25bb8e89670e3b038893973858fa43c7
David Winn <q@qypea.com>
2018-09-12 06:16:41 -0700

    backup: Mount and unmount sshfs

--- bin/backup.sh
+++ bin/backup.sh
@@ -2,8 +2,14 @@
 
 /home/q/bin/title.sh backup
 
+mkdir /tmp/backups
+sshfs leviathan:backups/samba /tmp/backups/
+
 rm -rf /home/q/.macromedia/Flash_Player
 cp /home/q/.bashrc /home/q/bin/bashrc
 kbackup --auto /home/q/.mybackup.kbp
+
+fusermount -u /tmp/backups
+rmdir /tmp/backups
 
 /home/q/bin/sync_media.sh
