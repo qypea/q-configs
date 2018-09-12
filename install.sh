ef13bf8328b994d0346d041cc27cc9f3d4f57328
David Winn <q@qypea.com>
2018-09-12 06:37:54 -0700

    desktop-configs: Install xfce config files

--- install.sh
+++ install.sh
@@ -5,10 +5,7 @@ STOWCMD="stow --target=$HOME"
 $STOWCMD vim-configs
 $STOWCMD git-configs
 $STOWCMD screen-configs
+$STOWCMD desktop-configs
 
 rm -rf ~/bin
 ln -s `pwd`/bin ~/bin
-
-mkdir -p ~/.config
-rm -rf ~/.config/xfce4
-ln -s `pwd`/config-xfce4 ~/.config/xfce4
