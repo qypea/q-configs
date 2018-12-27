0d8eee205e72078598837b8ad7100c89711b8dfb
David Winn <q@qypea.com>
2018-12-27 05:43:50 -0800

    Add date stamp to backups, reduce noise

--- home-bin/bin/backup.sh
+++ home-bin/bin/backup.sh
@@ -10,6 +10,8 @@ remoteHost="leviathan.local"
 remoteDir="/home/q/backups/sphinx"
 localDir="/home/q"
 
-ssh ${remoteHost} "rsync -av --delete --delete-after ${remoteDir}/backup.1/* ${remoteDir}/backup.2"
-ssh ${remoteHost} "rsync -av --delete --delete-after ${remoteDir}/backup.0/* ${remoteDir}/backup.1"
+date > ${localDir}/.lastbackup
+
+ssh ${remoteHost} "rsync -a --delete --delete-after ${remoteDir}/backup.1/* ${remoteDir}/backup.2"
+ssh ${remoteHost} "rsync -a --delete --delete-after ${remoteDir}/backup.0/* ${remoteDir}/backup.1"
 rsync -av --delete --delete-after ${localDir} ${remoteHost}:${remoteDir}/backup.0
