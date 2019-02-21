eb3ebf5742302fd61431264828a138096417cad2
David Winn <q@qypea.com>
2019-02-21 06:14:51 -0800

    Improve excludes in backup script

--- home-bin/bin/backup.sh
+++ home-bin/bin/backup.sh
@@ -11,11 +11,16 @@ common="--no-encryption --verbosity=info"
 
 duplicity cleanup ${common} ${remoteDir}
 
-duplicity ${common} --full-if-older-than=90D \
-    --exclude /home/q/.ssb/flume --exclude /home/q/.ssb/blobs \
+duplicity ${common} --full-if-older-than=30D \
+    --include /home/q/.ssb/secret --include /home/q/.ssb/gossip.json \
+    --exclude /home/q/.ssb \
     --exclude /home/q/.cache --exclude /home/q/.ccache \
-    --exclude /home/q/.atom/packages --exclude /home/q/.atom/.apm \
     --exclude /home/q/.config/\*/Cache \
+    --exclude /home/q/.thumbnails \
+    --exclude /home/q/.atom --exclude /home/q/.config/Atom \
+    --exclude /home/q/.adobe \
+    --exclude /home/q/.npm \
+    --exclude /home/q/temp \
     ${localDir} ${remoteDir}
 
 duplicity remove-older-than 90D ${common} --force ${remoteDir}
