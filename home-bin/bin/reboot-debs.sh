0a3acdd411c1022b0f15e463ea65d37bcac34677
David Winn <q@qypea.com>
2019-03-08 05:47:54 -0800

    Rename hassio-pi to hassiopi

--- home-bin/bin/reboot-debs.sh
+++ home-bin/bin/reboot-debs.sh
@@ -1,5 +1,5 @@
 
 ssh leviathan.local -t "./bin/shutdown-vms.sh && sudo shutdown -r +2"
 ssh scuttlepi.local -t "sudo shutdown -r +2"
-ssh hassio-pi.local -t "sudo shutdown -r +2"
+ssh hassiopi.local -t "sudo shutdown -r +2"
 ssh routepi.local -t "sudo reboot"
