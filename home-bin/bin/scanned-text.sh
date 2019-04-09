aae6c345ea20ace27420dcf248ea28381558a414
David Winn <q@qypea.com>
2019-04-09 05:30:43 -0700

    scanned-text: Script to pull text out of scanned files

---
+++ home-bin/bin/scanned-text.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+set -u
+set -x
+
+convert "$1" -colorspace Gray -depth 8 -average converted.pnm
+unpaper converted.pnm unpapered.pnm
+rm converted.pnm
+
+convert unpapered.pnm -normalize -density 300 prepared.tif
+rm unpapered.pnm
+
+tesseract prepared.tif "$1" -l eng
+rm prepared.tif
