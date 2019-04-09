575e4a0489d3ea43b39cda8abfa7807ce72e373e
David Winn <q@qypea.com>
2019-04-09 05:28:54 -0700

    Drop scuttlepi machine from scripts
    No longer used. Will repurpose it

--- home-bin/bin/update.sh
+++ home-bin/bin/update.sh
@@ -10,12 +10,3 @@ sudo apt autoclean
 sudo apt autoremove
 sudo apt update
 sudo apt dist-upgrade
-
-npm update -g ssb-server ssb-client scuttle-shell
-npm update --registry=http://localhost:8043/ -g git-ssb ssb-npm
-
-pushd ~/.ssb/node_modules/patchfoo
-git pull
-rm -f package-lock.json
-npm install --registry=http://localhost:8043/
-popd
