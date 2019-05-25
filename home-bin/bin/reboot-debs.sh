1c24050e98f7a63f9336bdbaa9b07f03d785ea03
David Winn <q@qypea.com>
2019-05-25 05:31:39 -0700

    reboot-debs: Update for new router setup

--- home-bin/bin/reboot-debs.sh
+++ home-bin/bin/reboot-debs.sh
@@ -1,4 +1,4 @@
 
-ssh leviathan.local -t "./bin/shutdown-vms.sh && sudo shutdown -r +2"
-ssh hassiopi.local -t "sudo shutdown -r +2"
-ssh routepi.local -t "sudo reboot"
+ssh leviathan.lan -t "./bin/shutdown-vms.sh && sudo shutdown -r +2"
+ssh hassiopi.lan -t "sudo shutdown -r +2"
+#ssh routepi.local -t "sudo reboot"
