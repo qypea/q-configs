bf71d3f5d06513df9c682f8e8246c8972a89556a
David Winn <q@qypea.com>
2022-07-30 16:00:25 -0700

    update script: Update snaps too

--- common-bin/bin/update.sh
+++ common-bin/bin/update.sh
@@ -7,6 +7,8 @@ sudo apt autoclean
 sudo apt autoremove
 sudo apt update
 sudo apt dist-upgrade
+sudo snap refresh --list
+sudo snap refresh
 
 if [ -f /var/run/reboot-required ]; then
         cat /var/run/reboot-required
