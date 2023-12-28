1b1454a5865f2b392d1dc501f85138ad801067c2
David Winn <q@qypea.com>
2023-12-28 13:27:16 -0800

    nvim: Break cmp into its own file
    Easier to have other things also plug into it if it is separate.

---
+++ common-nvim/.config/nvim/lua/plugins/cmp.lua
@@ -0,0 +1,28 @@
+return {
+    {
+        -- Autocompletion
+        'hrsh7th/nvim-cmp',
+        dependencies = {
+            { 'L3MON4D3/LuaSnip' },
+            { 'VonHeikemen/lsp-zero.nvim' },
+        },
+        config = function()
+            local lsp_zero = require('lsp-zero')
+            lsp_zero.extend_cmp()
+
+            local cmp = require('cmp')
+            local cmp_action = lsp_zero.cmp_action()
+
+            cmp.setup({
+                formatting = lsp_zero.cmp_format(),
+                mapping = cmp.mapping.preset.insert({
+                    -- Enter to confirm completion
+                    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                    -- Super tab
+                    ['<Tab>'] = cmp_action.luasnip_supertab(),
+                    ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+                })
+            })
+        end
+    },
+}
--- common-nvim/.config/nvim/lua/plugins/lsp.lua
+++ common-nvim/.config/nvim/lua/plugins/lsp.lua
@@ -4,29 +4,6 @@ return {
         'VonHeikemen/lsp-zero.nvim',
     },
     {
-        -- Autocompletion
-        'hrsh7th/nvim-cmp',
-        dependencies = { { 'L3MON4D3/LuaSnip' }, },
-        config = function()
-            local lsp_zero = require('lsp-zero')
-            lsp_zero.extend_cmp()
-
-            local cmp = require('cmp')
-            local cmp_action = lsp_zero.cmp_action()
-
-            cmp.setup({
-                formatting = lsp_zero.cmp_format(),
-                mapping = cmp.mapping.preset.insert({
-                    -- Enter to confirm completion
-                    ['<CR>'] = cmp.mapping.confirm({ select = true }),
-                    -- Super tab
-                    ['<Tab>'] = cmp_action.luasnip_supertab(),
-                    ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
-                })
-            })
-        end
-    },
-    {
         -- LSP servers
         'neovim/nvim-lspconfig',
         dependencies = { { 'hrsh7th/cmp-nvim-lsp' }, },
