0c50123e2b45878c8a9788b2c19b32928ff44baf
David Winn <q@qypea.com>
2023-12-28 07:49:44 -0800

    nvim: Disable stylua formatting
    Handled by lua language server

--- common-nvim/.config/nvim/lua/plugins/init.lua
+++ common-nvim/.config/nvim/lua/plugins/init.lua
@@ -4,8 +4,8 @@ return {
         'ethanholz/nvim-lastplace',
         config = function()
             require('nvim-lastplace').setup({
-                lastplace_ignore_buftype = {"quickfix", "nofile", "help"},
-                lastplace_ignore_filetype = {"gitcommit", "gitrebase", "svn", "hgcommit"},
+                lastplace_ignore_buftype = { "quickfix", "nofile", "help" },
+                lastplace_ignore_filetype = { "gitcommit", "gitrebase", "svn", "hgcommit" },
                 lastplace_open_folds = true
             })
         end,
--- common-nvim/.config/nvim/lua/plugins/lsp.lua
+++ common-nvim/.config/nvim/lua/plugins/lsp.lua
@@ -59,7 +59,6 @@ return {
             local null_ls = require("null-ls")
             return {
                 sources = {
-                    null_ls.builtins.formatting.stylua,
                     -- Python Styling
                     null_ls.builtins.diagnostics.pydocstyle.with({
                         method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
