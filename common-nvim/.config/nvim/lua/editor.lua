2a82af2a6dcb5a965905dab02d0b3f00289e5702
David Winn <q@qypea.com>
2025-03-31 14:08:21 -0700

    nvim: Indent based on .indent files

--- common-nvim/.config/nvim/lua/editor.lua
+++ common-nvim/.config/nvim/lua/editor.lua
@@ -23,11 +23,24 @@ vim.api.nvim_command('highlight SpellCap cterm=underline')
 -- Don't start new lines with comments
 vim.api.nvim_command('au FileType * set fo-=c fo-=r fo-=o')
 
--- Spaces, 4 wide with tab char
--- TODO: Indent amount based on .files in working copy
-vim.opt.expandtab = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
+-- Indent based on the .indent file closest to this working copy
+local indent_file = vim.fs.find({ '.indent_2', '.indent_4', '.indent_tab' },
+    { type = 'file', follow = true, limit = 1, upward = true })[1]
+if indent_file == nil or vim.fs.basename(indent_file) == '.indent_2' then
+    vim.opt.expandtab = true
+    vim.opt.tabstop = 2
+    vim.opt.shiftwidth = 2
+elseif vim.fs.basename(indent_file) == '.indent_4' then
+    vim.opt.expandtab = true
+    vim.opt.tabstop = 4
+    vim.opt.shiftwidth = 4
+elseif vim.fs.basename(indent_file) == '.indent_tab' then
+    vim.opt.expandtab = false
+    vim.opt.tabstop = 8
+    vim.opt.shiftwidth = 8
+else
+    vim.notify("Invalid notify file found. Defaults may not be good", vim.log.levels.ERROR)
+end
 
 vim.api.nvim_create_user_command("Dwtasks",
     function(opts)
