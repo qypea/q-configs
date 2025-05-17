4d8d573fdd1ff9acd55f24f1d2becac100f10dcf
David Winn <q@qypea.com>
2025-05-17 15:33:24 -0700

    update: Switch over to nixos update script

--- common-bin/bin/update.sh
+++ common-bin/bin/update.sh
@@ -3,14 +3,6 @@
 set -e
 set -u
 
-sudo apt autoclean
-sudo apt autoremove
-sudo apt update
-sudo apt dist-upgrade
-sudo snap refresh --list
-sudo snap refresh
-
-if [ -f /var/run/reboot-required ]; then
-        cat /var/run/reboot-required
-fi
-uptime
+nix-collect-garbage --delete-old
+nixos-rebuild switch --use-remote-sudo
+nix-store --optimise
